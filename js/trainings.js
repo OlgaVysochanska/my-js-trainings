fc48a9d7287f869f9297b59a7ac99adee9e519ce
OlgaVysochanska <olavysochanska@gmail.com>
2022-11-19 00:40:07 +0200

    try catch remove from locatStor

--- js/trainings.js
+++ js/trainings.js
@@ -1,1 +1,10 @@
 console.log("hi");
+
+const load = key => {
+  try {
+    const erializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : localStorage.removeItem(key);
+  } catch (error) {
+    console.error("Get state error: ", error.message);
+  }
+};
