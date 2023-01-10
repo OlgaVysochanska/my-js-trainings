d2ea883006c6f72cdd88cd9be5dd9665af623a2e
OlgaVysochanska <olavysochanska@gmail.com>
2023-01-10 22:02:26 +0200

    validate pin codewars

--- js/trainings.js
+++ js/trainings.js
@@ -1,10 +1,26 @@
-console.log("hi there");
+// console.log("hi there");
 
-const load = key => {
-  try {
-    const erializedState = localStorage.getItem(key);
-    return serializedState === null ? undefined : localStorage.removeItem(key);
-  } catch (error) {
-    console.error("Get state error: ", error.message);
+// const load = key => {
+//   try {
+//     const erializedState = localStorage.getItem(key);
+//     return serializedState === null ? undefined : localStorage.removeItem(key);
+//   } catch (error) {
+//     console.error("Get state error: ", error.message);
+//   }
+// };
+
+// codewars: validate Pin code
+
+function validatePIN(str) {
+  if (str.length !== 4 && str.length !== 6) {
+    return false;
   }
-};
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] < '0' || str[i] > '9') {
+      return false;
+    }
+  }
+  return true;
+}
+
+validatePIN('5768yo');
